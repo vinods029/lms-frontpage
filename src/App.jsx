245ac037a271ab5f8b5a07f75e23ae79d1d2cdc9
vinods029 <vinods029@gmail.com>
2023-11-14 22:02:15 +0530

    added react hot toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 
 import './App.css'
 
+import { useEffect } from 'react'
+import toast from 'react-hot-toast'
+
 function App() {
+
+  useEffect(()=>{
+    toast.success("success")
+  })
 
 
   return (
