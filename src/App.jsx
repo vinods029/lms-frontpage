2f5af13604ff4aef6906dfc2339c573649771ba6
vinods029 <vinods029@gmail.com>
2023-11-22 21:26:43 +0530

    Not found page was added

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Aboutus from "./pages/Aboutus";
 import Home from "./pages/Home";
+import Notfound from "./pages/Notfound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<Aboutus />} />
+      <Route path="*" element={<Notfound />} />
     </Routes>
   );
 }
